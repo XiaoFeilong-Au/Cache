dc3406649888d86f15d8c2658897e03dfe48a1dc
XiaoFeilong-Au <2101667816@qq.com>
2023-02-14 14:49:56 +0800

    new file:   consistenthash/consistenthash.go 	new file:   consistenthash/consistenthash_test.go 	new file:   http.go 	modified:   mycache.go 	modified:   mycache_test.go 	new file:   mycachepb/geecachepb.pb.go 	new file:   mycachepb/geecachepb.proto 	new file:   peers.go

--- mycache.go
+++ mycache.go
@@ -2,9 +2,22 @@ package mycache
 
 import (
 	"fmt"
+	pb "geecache/geecachepb"
+	"geecache/singleflight"
 	"log"
 	"sync"
 )
+
+// A Group is a cache namespace and associated data loaded spread over
+type Group struct {
+	name      string
+	getter    Getter
+	mainCache cache
+	peers     PeerPicker
+	// use singleflight.Group to make sure that
+	// each key is only fetched once
+	loader *singleflight.Group
+}
 
 // A Getter loads data for a key.
 type Getter interface {
@@ -17,13 +30,6 @@ type GetterFunc func(key string) ([]byte, error)
 // Get implements Getter interface function
 func (f GetterFunc) Get(key string) ([]byte, error) {
 	return f(key)
-}
-
-// A Group is a cache namespace and associated data loaded spread over
-type Group struct {
-	name      string
-	getter    Getter
-	mainCache cache
 }
 
 var (
@@ -42,6 +48,7 @@ func NewGroup(name string, cacheBytes int64, getter Getter) *Group {
 		name:      name,
 		getter:    getter,
 		mainCache: cache{cacheBytes: cacheBytes},
+		loader:    &singleflight.Group{},
 	}
 	groups[name] = g
 	return g
@@ -70,8 +77,38 @@ func (g *Group) Get(key string) (ByteView, error) {
 	return g.load(key)
 }
 
+// RegisterPeers registers a PeerPicker for choosing remote peer
+func (g *Group) RegisterPeers(peers PeerPicker) {
+	if g.peers != nil {
+		panic("RegisterPeerPicker called more than once")
+	}
+	g.peers = peers
+}
+
 func (g *Group) load(key string) (value ByteView, err error) {
-	return g.getLocally(key)
+	// each key is only fetched once (either locally or remotely)
+	// regardless of the number of concurrent callers.
+	viewi, err := g.loader.Do(key, func() (interface{}, error) {
+		if g.peers != nil {
+			if peer, ok := g.peers.PickPeer(key); ok {
+				if value, err = g.getFromPeer(peer, key); err == nil {
+					return value, nil
+				}
+				log.Println("[GeeCache] Failed to get from peer", err)
+			}
+		}
+
+		return g.getLocally(key)
+	})
+
+	if err == nil {
+		return viewi.(ByteView), nil
+	}
+	return
+}
+
+func (g *Group) populateCache(key string, value ByteView) {
+	g.mainCache.add(key, value)
 }
 
 func (g *Group) getLocally(key string) (ByteView, error) {
@@ -85,6 +122,15 @@ func (g *Group) getLocally(key string) (ByteView, error) {
 	return value, nil
 }
 
-func (g *Group) populateCache(key string, value ByteView) {
-	g.mainCache.add(key, value)
+func (g *Group) getFromPeer(peer PeerGetter, key string) (ByteView, error) {
+	req := &pb.Request{
+		Group: g.name,
+		Key:   key,
+	}
+	res := &pb.Response{}
+	err := peer.Get(req, res)
+	if err != nil {
+		return ByteView{}, err
+	}
+	return ByteView{b: res.Value}, nil
 }
--- mycache_test.go
+++ mycache_test.go
@@ -1,8 +1,9 @@
 package mycache
 
-import(
-	"log"
+import (
 	"fmt"
+	"log"
+	"reflect"
 	"testing"
 )
 
@@ -10,6 +11,17 @@ var db = map[string]string{
 	"Tom":  "630",
 	"Jack": "589",
 	"Sam":  "567",
+}
+
+func TestGetter(t *testing.T) {
+	var f Getter = GetterFunc(func(key string) ([]byte, error) {
+		return []byte(key), nil
+	})
+
+	expect := []byte("key")
+	if v, _ := f.Get("key"); !reflect.DeepEqual(v, expect) {
+		t.Fatal("callback failed")
+	}
 }
 
 func TestGet(t *testing.T) {
@@ -21,7 +33,7 @@ func TestGet(t *testing.T) {
 				if _, ok := loadCounts[key]; !ok {
 					loadCounts[key] = 0
 				}
-				loadCounts[key] += 1
+				loadCounts[key]++
 				return []byte(v), nil
 			}
 			return nil, fmt.Errorf("%s not exist", key)
@@ -30,13 +42,26 @@ func TestGet(t *testing.T) {
 	for k, v := range db {
 		if view, err := gee.Get(k); err != nil || view.String() != v {
 			t.Fatal("failed to get value of Tom")
-		} // load from callback function
+		}
 		if _, err := gee.Get(k); err != nil || loadCounts[k] > 1 {
 			t.Fatalf("cache %s miss", k)
-		} // cache hit
+		}
 	}
 
 	if view, err := gee.Get("unknown"); err == nil {
 		t.Fatalf("the value of unknow should be empty, but %s got", view)
+	}
+}
+
+func TestGetGroup(t *testing.T) {
+	groupName := "scores"
+	NewGroup(groupName, 2<<10, GetterFunc(
+		func(key string) (bytes []byte, err error) { return }))
+	if group := GetGroup(groupName); group == nil || group.name != groupName {
+		t.Fatalf("group %s not exist", groupName)
+	}
+
+	if group := GetGroup(groupName + "111"); group != nil {
+		t.Fatalf("expect nil, but %s got", group.name)
 	}
 }
